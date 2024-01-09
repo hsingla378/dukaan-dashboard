6b87ce48bb6e414c8145e6f665adf702c6c37060
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-01-09 11:06:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Payouts from "./components/Payouts";
+
+function App() {
+  return (
+    <>
+      <Payouts />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Payouts.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Payouts = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <div>
+        {/* Navbar Header */}
+        <div></div>
+        {/* Navbar Links */}
+        <div></div>
+        {/* Navbar Footer - Available Credits */}
+        <div></div>
+      </div>
+      {/* Dashboard */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Payouts;
