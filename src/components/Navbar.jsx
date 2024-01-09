83ccc6579d56abbeaf718f71a2bbdb9e3c0e5408
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-01-09 23:22:58 +0530

    add nav links conditional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,22 @@
+import React, { useState } from "react";
 import shopLogo from "./../assets/shop_logo.png";
 import { FaAngleDown } from "react-icons/fa";
-import { GoHome } from "react-icons/go";
-import { LuClipboardList } from "react-icons/lu";
-import { TiThLargeOutline } from "react-icons/ti";
-import { CiDeliveryTruck } from "react-icons/ci";
-import { TfiAnnouncement } from "react-icons/tfi";
-import { TbBrandGoogleAnalytics } from "react-icons/tb";
-import { BiSolidOffer } from "react-icons/bi";
-import { FaMoneyBillTransfer } from "react-icons/fa6";
-import { GoPeople } from "react-icons/go";
-import { IoColorPaletteOutline } from "react-icons/io5";
-import { MdElectricBolt } from "react-icons/md";
 import { RiWallet3Line } from "react-icons/ri";
+import { pageLinks } from "../utils/constants";
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState("Home");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div className="p-3 flex flex-col justify-between h-screen text-white fixed bg-[#1E2640] w-[inherit] cursor-pointer">
+    <div className="p-3 flex flex-col justify-between h-screen text-white fixed bg-[#1E2640] w-[inherit] ">
       {/* Nav Top */}
       <div>
         {/* Nav Header */}
-        <div className="flex items-center justify-between p-2">
+        <div className="flex items-center justify-between p-2 cursor-pointer">
           <div className="flex justify-between items-center gap-3">
             <img src={shopLogo} alt="Nishyan" className="w-[39px]" />
             <div className="flex flex-col gap-0.5">
@@ -33,50 +30,20 @@ function Navbar() {
         </div>
         {/* Pages Links */}
         <div className="mt-4 text-gray-300 font-semibold">
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <GoHome className="text-[16.96px]" />
-            <p className="text-[14px]">Home</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <LuClipboardList className="text-[16.96px]" />
-            <p className="text-[14px]">Orders</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <TiThLargeOutline className="text-[16.96px]" />
-            <p className="text-[14px]">Products</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <CiDeliveryTruck className="text-[16.96px]" />
-            <p className="text-[14px]">Delivery</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <TfiAnnouncement className="text-[16.96px]" />
-            <p className="text-[14px]">Marketing</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <TbBrandGoogleAnalytics className="text-[16.96px]" />
-            <p className="text-[14px]">Analytics</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center rounded-md bg-white bg-opacity-10 text-gray-200">
-            <FaMoneyBillTransfer className="text-[16.96px]" />
-            <p className="text-[14px]">Payouts</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <BiSolidOffer className="text-[16.96px]" />
-            <p className="text-[14px]">Discounts</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <GoPeople className="text-[16.96px]" />
-            <p className="text-[14px]">Audience</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <IoColorPaletteOutline className="text-[16.96px]" />
-            <p className="text-[14px]">Appearance</p>
-          </div>
-          <div className="flex gap-3 py-2 px-3 items-center">
-            <MdElectricBolt className="text-[16.96px]" />
-            <p className="text-[14px]">Plugins</p>
-          </div>
+          {pageLinks.map((item) => (
+            <div
+              key={item.text}
+              className={`flex gap-3 py-2 px-3 items-center cursor-pointer ${
+                activeLink === item.text
+                  ? "rounded-md bg-white bg-opacity-10 text-gray-200"
+                  : ""
+              }`}
+              onClick={() => handleLinkClick(item.text)}
+            >
+              {item.icon}
+              <p className="text-[14px]">{item.text}</p>
+            </div>
+          ))}
         </div>
       </div>
       {/* Nav Bottom - Available Credits  */}
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,15 @@
+import { GoHome } from "react-icons/go";
+import { LuClipboardList } from "react-icons/lu";
+import { TiThLargeOutline } from "react-icons/ti";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { TfiAnnouncement } from "react-icons/tfi";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { BiSolidOffer } from "react-icons/bi";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { GoPeople } from "react-icons/go";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { MdElectricBolt } from "react-icons/md";
+
 export const transactions = [
   {
     orderId: 281209,
@@ -40,5 +52,46 @@ export const transactions = [
     transactionId: 131534495747,
     refundDate: "12 Jul 2023, 03:00 PM",
     orderAmount: 1125.0,
+  },
+];
+
+export const pageLinks = [
+  { icon: <GoHome className="text-[16.96px]" />, text: "Home" },
+  {
+    icon: <LuClipboardList className="text-[16.96px]" />,
+    text: "Orders",
+  },
+  {
+    icon: <TiThLargeOutline className="text-[16.96px]" />,
+    text: "Products",
+  },
+  {
+    icon: <CiDeliveryTruck className="text-[16.96px]" />,
+    text: "Delivery",
+  },
+  {
+    icon: <TfiAnnouncement className="text-[16.96px]" />,
+    text: "Marketing",
+  },
+  {
+    icon: <TbBrandGoogleAnalytics className="text-[16.96px]" />,
+    text: "Analytics",
+  },
+  {
+    icon: <FaMoneyBillTransfer className="text-[16.96px]" />,
+    text: "Payouts",
+  },
+  {
+    icon: <BiSolidOffer className="text-[16.96px]" />,
+    text: "Discounts",
+  },
+  { icon: <GoPeople className="text-[16.96px]" />, text: "Audience" },
+  {
+    icon: <IoColorPaletteOutline className="text-[16.96px]" />,
+    text: "Appearance",
+  },
+  {
+    icon: <MdElectricBolt className="text-[16.96px]" />,
+    text: "Plugins",
   },
 ];
